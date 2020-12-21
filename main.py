cec81d79bf1d39dfb5200be29d85fc2136388440
sumityadav1509 <sumityadav2005@gmail.com>
2020-12-21 21:43:51 +0000

    Heads or Tails random number generation using .seed() function exercise completed.

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 #Write your code below this line 👇
 #Hint: Remember to import the random module first. 🎲
 
+import random 
 
+test_seed=int(input("Create a seed number : ")) 
+random.seed(test_seed)
 
+random_int=random.randint(0, 1) 
+
+if random_int==1:
+  print("Heads") 
+else : 
+  print("Tails")
 
 
 
